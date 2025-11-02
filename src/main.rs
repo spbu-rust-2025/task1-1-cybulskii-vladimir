504b544d3cc2c40f8e8c1637b9220b96db1b8a8d
Waldemar <superlexaan@gmail.com>
2025-11-02 18:09:20 +0100

    make the first file for the task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut twonumbers = String::new();
+    io::stdin()
+        .read_line(&mut twonumbers)
+        .expect("Failed to read line");
+    let mut x = 0;
+    for i in twonumbers.split(' ') {
+        let inum : i32 = match i.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        x = x+inum;
+    }
+    println!("{x}");
 }
